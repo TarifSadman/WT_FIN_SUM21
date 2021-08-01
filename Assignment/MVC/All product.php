6833d9a47e1b203c812b33a54601a10a1f42f75e
TarifSadman228 <sadmantaha9991@gmail.com>
2021-08-01 17:05:05 +0600

    labaftereid2mvc

--- Assignment/MVC/All product.php
+++ Assignment/MVC/All product.php
@@ -1,29 +1,45 @@
+<?php
+    require_once 'Controllers/productcontroller.php';
+    $products= getAllProducts();
+?>
 <html>
-<head>
-<title>All Products</title>
-</head>
-<body>
-<fieldset>
-<table align = "center">
-<td><h2>All products</h2></td>
-</table>
-<table align = "center" border = "1">
-<tr>
-    <th>Sno</th>
-    <th>Name</th>
-    <th>Count</th>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>Mobile phone</td>
-    <td>50</td>
-  </tr>
-  <tr>
-    <td>2</td>
-    <td>Headphone</td>
-    <td>94</td>
-  </tr>
-</table>
-</fieldset>
-</body>
+	<head></head>
+	<body>
+        
+		<fieldset>
+		    <legend align="center"><h1> All Categories:</h1></legend>
+            <h5><?php echo $err_db; ?></h5>
+			<form action="" method="post">
+				<table align="center" >
+					<tr>
+					    <td><b><i>Sl#: <i><b></td>
+					    <td><b><i>Name: <i><b></td>
+                        <td><b><i>Category: <i><b></td>
+                        <td><b><i>Price: <i><b></td>
+                        <td><b><i>Quantity: <i><b></td>
+						
+						
+					</tr>
+
+                    <?php
+                        $i=1;
+					    foreach($products as $p){
+                            echo "<tr>";
+                                echo"<td>$i</td>";
+                                echo"<td><img src='".$p["image"]."' width='100px' height='100px'></td>";
+                                echo"<td>".$p["name"]."</td>";
+                                echo"<td>".$p["c_id"]."</td>";
+                                echo"<td>".$p["price"]."</td>";
+                                echo"<td>".$p["quantity"]."</td>";
+                                echo'<td ><a href="edit_product.php?id='.$p["id"].'"class="btn-btn-success">Edit</a></td>';
+                                echo'<td ><a href="delete_product.php?id='.$c["id"].'"class="btn-btn-success">Delete</a></td>';
+                            echo "<tr>";
+                            $i++;
+                        }
+					?>
+				</table>
+			</form>
+		</fieldset>
+		
+	</body>
 </html>
